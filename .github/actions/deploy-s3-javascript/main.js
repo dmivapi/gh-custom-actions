649cafd17e2b71a4e4f096b3148f59e230e982df
Dmytro Pishchanetskyi <56348904+dmivapi@users.noreply.github.com>
2026-06-01 16:09:51 +0300

    added output

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -8,9 +8,10 @@ function run() {
     const distFolder = core.getInput('dist-folder', { required: true });
 
     const s3Uri = `s3://${bucket}`
-    exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`)
+    exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`);
 
-    core.notice('Hello from my custom JavaScript Action!');
+    const websiteUrl = `http://${bucket}.s3-website.${bucketRegion}.amazonaws.com`;
+    core.setOutput('website-url', websiteUrl); // echo "website-url=${{ ... }}" >> $GITHUB_OUTPUT
 }
 
 run();
