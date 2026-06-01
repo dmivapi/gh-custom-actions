7953f7e1d5e1efb7d3bad58a4396cbfca89bec6e
Dmytro Pishchanetskyi <56348904+dmivapi@users.noreply.github.com>
2026-06-01 15:51:05 +0300

    S3 custom uploading added.

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -3,6 +3,13 @@ import * as github from '@actions/github';
 import * as exec from '@actions/exec';
 
 function run() {
+    const bucket = core.getInput('bucket', { required: true });
+    const bucketRegion = core.getInput('bucket-region', { required: true });
+    const distFolder = core.getInput('dist-folder', { required: true });
+
+    const s3Uri = `s3://${bucket}`
+    exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`)
+
     core.notice('Hello from my custom JavaScript Action!');
 }
 
